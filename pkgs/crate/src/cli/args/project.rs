43953528ecb2624aaed4392c5b60aaf07e4effb8
Sasha Koss <koss@nocorp.me>
2026-09-03 08:49:37 +0800

    Set up examples build

--- pkgs/crate/src/cli/args/project.rs
+++ pkgs/crate/src/cli/args/project.rs
@@ -4,5 +4,5 @@ use crate::cli::prelude::*;
 pub struct EscCliArgsProject {
     /// Use the given project or `tsconfig.json` path instead of the cwd.
     #[usage(short, long, value_name = "PROJECT_PATH")]
-    pub project_path: Option<PathBuf>,
+    pub project: Option<PathBuf>,
 }
